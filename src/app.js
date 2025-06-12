4dab84b191214578b5c0f134286db6eda032b712
Krishna Vamsi Pinninti <krishpinninti789@gmail.com>
2025-06-12 19:35:06 +0530

    implemented cors for frontend

--- src/app.js
+++ src/app.js
@@ -2,9 +2,16 @@ const express = require("express");
 const app = express();
 const connectToDB = require("./config/db");
 const cookieparser = require("cookie-parser");
+const cors = require("cors");
 
 app.use(express.json());
 app.use(cookieparser());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
 const authRouter = require("./routes/auth");
 const profileRouter = require("./routes/profile");
@@ -17,7 +24,7 @@ app.use("/", userRouter);
 
 connectToDB()
   .then(() => {
-    console.log("COnnceted to db");
+    console.log("Connected to db");
     app.listen(3000, () => {
       console.log("server is running on port 3000");
     });
